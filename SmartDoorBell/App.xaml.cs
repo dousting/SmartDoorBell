81d4f062de9f95382ecbda7e31a927a41a81a41a
[dousting] <dcalder1@mycambrian.ca>
2024-02-11 11:22:35 -0500

    Add project files.

---
+++ SmartDoorBell/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartDoorBell
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
