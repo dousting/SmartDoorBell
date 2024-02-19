7e83c8b23505e6b12338daf4f6f30878b8096c4e
[dousting] <dcalder1@mycambrian.ca>
2024-02-19 14:07:22 -0500

    Version 0: Login page, user, and password as credentials. Main Page: six buttons distributed.
    To do: implement the functions of each button.

--- SmartDoorBell/App.xaml.cs
+++ SmartDoorBell/App.xaml.cs
@@ -1,12 +1,19 @@
-﻿namespace SmartDoorBell
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace SmartDoorBell
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+        }
 
-            MainPage = new AppShell();
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            // Set the login page as the initial page
+            return new Window(new NavigationPage(new LoginPage()));
         }
     }
 }
